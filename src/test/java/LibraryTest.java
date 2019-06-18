ef2010505c69c24adf26dbb737ec7ae7d17188a4
thomas <thomas@PC-Thomas>
2019-06-18 14:55:17 +0200

    mise à jour config

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -3,6 +3,8 @@
  */
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
