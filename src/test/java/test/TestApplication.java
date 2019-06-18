6e722b98b411e6421202e33eab4b1a0734f67ba6
thomas <thomas@PC-Thomas>
2019-06-18 15:35:27 +0200

    Premier test

---
+++ src/test/java/test/TestApplication.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import calculatrice.Application;
+
+public class TestApplication {
+	
+	@Test
+    public void test() {
+       int a = 1;
+       int b = 2;
+
+       int result = Application.add(a, b);
+
+       Assert.assertEquals(3, result);
+       System.out.println("test ok");
+    }
+
+}
