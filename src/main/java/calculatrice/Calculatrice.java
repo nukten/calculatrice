4d6f158233bf4cec2511f4dd03a225f00b5528fd
thomas <thomas@PC-Thomas>
2019-06-19 21:39:20 +0200

    Sprint 0.2.0 avec tests

--- src/main/java/calculatrice/Calculatrice.java
+++ src/main/java/calculatrice/Calculatrice.java
@@ -18,13 +18,20 @@ public class Calculatrice implements ICalculatrice {
 			return "Erreur : Une des saisies n'a pas le type attendu.";
 		}
 	}
-	
 
 	@Override
 	public String soustraction(String a_premiereValeur, String a_deuxiemeValeur) {
-		float l_premiereValeur = Float.valueOf(a_premiereValeur);
-		float l_deuxiemeValeur = Float.valueOf(a_deuxiemeValeur);
-		return String.valueOf(l_premiereValeur - l_deuxiemeValeur);
+		try {
+			float l_premiereValeur = Float.valueOf(a_premiereValeur);
+			float l_deuxiemeValeur = Float.valueOf(a_deuxiemeValeur);
+			if (l_premiereValeur >= 0 & l_deuxiemeValeur >= 0) {
+				return String.valueOf(l_premiereValeur - l_deuxiemeValeur);
+			} else {
+				return "Erreur : Un nombre est inférieur à 0.";
+			}
+		} catch (NumberFormatException e) {
+			return "Erreur : Une des saisies n'a pas le type attendu.";
+		}
 	}
 
 	/**
@@ -50,6 +57,5 @@ public class Calculatrice implements ICalculatrice {
 	/**
 	 * {@inheritDoc}
 	 */
-
 
 }
