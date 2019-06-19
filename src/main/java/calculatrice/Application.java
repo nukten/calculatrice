0669a074c884757ad0acd404e50af8e485a0076e
thomas <thomas@PC-Thomas>
2019-06-19 11:16:04 +0200

    Sprint 0.1.0

--- src/main/java/calculatrice/Application.java
+++ src/main/java/calculatrice/Application.java
@@ -1,13 +1,47 @@
 package calculatrice;
 
+import java.util.Scanner;
+
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello momo");
+		
+		ICalculatrice vCalculator =  new Calculatrice();
+		
+		// Now interact with user!
+		Scanner sc = new Scanner(System.in);
+		//while(true) {
+			System.out.println("Valeur 1 : ");
+			String premiereValeur = sc.nextLine();
+			
+			System.out.println("Valeur 2: ");
+			String deuxiemeValeur = sc.nextLine();
+			
+			System.out.println("L'opération (addition ) ? ");
+			String vOperationKey = sc.nextLine();
+			
+			String resultat = Constantes.EMPTY;
+			
+			switch (vOperationKey) {
+			case Constantes.ADD_KEY:
+				resultat = vCalculator.addition(premiereValeur, deuxiemeValeur);
+				break;
+			default:
+				resultat = "Operation not found";
+				break;
+		}
+			
+			// Do the operation and show results
+			System.out.println("Le résultat est : ");
+			System.out.println(resultat);
+			
+			sc.nextLine();
+			
+			System.out.println("----------------------------------------------------------------");
+
+		//}
 	}
 	
-	public static int add(int a, int b) {
-        return a + b;
-    }
+	
 }
---
+++ src/main/java/calculatrice/Calculatrice.java
@@ -0,0 +1,45 @@
+package calculatrice;
+
+public class Calculatrice implements ICalculatrice {
+	
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String addition(String a_premiereValeur, String a_deuxiemeValeur) {
+		float l_premiereValeur = Float.valueOf(a_premiereValeur);
+		float l_deuxiemeValeur = Float.valueOf(a_deuxiemeValeur);
+		//return String.valueOf(l_premiereValeur + l_deuxièmeValeur);
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String division(String a_premiereValeur, String a_deuxiemeValeur) {
+		float l_premiereValeur = Float.valueOf(a_premiereValeur);
+		float l_deuxiemeValeur = Float.valueOf(a_deuxiemeValeur);
+		return String.valueOf(l_premiereValeur / l_deuxiemeValeur);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String multiplication(String a_premiereValeur, String a_deuxiemeValeur) {
+		float l_premiereValeur = Float.valueOf(a_premiereValeur);
+		float l_deuxiemeValeur = Float.valueOf(a_deuxiemeValeur);
+		return String.valueOf(l_premiereValeur*l_deuxiemeValeur);	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String soustraction(String a_premiereValeur, String a_deuxiemeValeur) {
+		float l_premiereValeur = Float.valueOf(a_premiereValeur);
+		float l_deuxiemeValeur = Float.valueOf(a_deuxiemeValeur);
+		return String.valueOf(l_premiereValeur-l_deuxiemeValeur);	}
+
+}
