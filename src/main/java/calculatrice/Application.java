72957b89acd9e7ad7ec27a7ba293f6ceb9f9d5ee
titome49 <tlg.thomaslegall@gmail.com>
2019-06-20 10:03:46 +0200

    Update Application.java

--- src/main/java/calculatrice/Application.java
+++ src/main/java/calculatrice/Application.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Application {
 
-  public static void main(String[] args) {
+   public static void main(String[] args) {
 
 		ICalculatrice vCalculator = new Calculatrice();
 		Scanner sc = new Scanner(System.in);
