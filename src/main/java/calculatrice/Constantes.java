5220dad99ed9d5f374647c31e3866cdcf03e9505
nukten <toto61300@gmail.com>
2019-06-20 15:43:06 +0200

    Tout ça c'est la faute de Thomas

--- src/main/java/calculatrice/Constantes.java
+++ src/main/java/calculatrice/Constantes.java
@@ -4,25 +4,25 @@ public class Constantes {
 
 public static final String EMPTY = "";
 
-	/** Le séparateur de représentation pour le float. */
+	/** Le separateur de représentation pour le float. */
 	public static final String FLOAT_SEPARATOR = ".";
 
-	/** La clé pour l'opération d'addition. */
+	/** La cle pour l'opération d'addition. */
 	public static final String ADD_KEY = "+";
 
-	/** La clé pour l'opération soustraction. */
+	/** La cle pour l'opération soustraction. */
 	public static final String SUB_KEY = "-";
 
-	/** La clé pour l'opération division. */
+	/** La cle pour l'opération division. */
 	public static final String DIVIDE_KEY = "/";
 
-	/** La clé pour l'opération multiplication. */
+	/** La cle pour l'opération multiplication. */
 	public static final String MUL_KEY = "*";
 
-	/** La clé pour pour quitter l'application. */
+	/** La cle pour pour quitter l'application. */
 	public static final String QUIT_APPLI = "q";
 
-	/** La cl� pour l'historique */
+	/** La cle pour l'historique */
 	public static final String HISTO = "h";
 
 
