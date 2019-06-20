80e9062474b9f4f55b931ec4c67bb3f9463b7408
titome49 <tlg.thomaslegall@gmail.com>
2019-06-20 09:17:45 +0200

    Test de commit

--- src/main/java/calculatrice/Application.java
+++ src/main/java/calculatrice/Application.java
@@ -13,7 +13,7 @@ public class Application {
 		String deuxiemeValeur = null;
 		String resultat = null;
 
-		do {
+		do { //
 			System.out.println("Choissisez l'opération (+/-/*) ou q pour quitter ! ");
 			String operation = sc.nextLine();
 
