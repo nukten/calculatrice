d7d9f16b7eb526d29cbcaeeb97dc439f02078a3b
Arnaud RIDÉ <arnaud.ride@outlook.fr>
2019-06-21 10:27:40 +0200

    corrections Checkstyle

--- src/main/java/calculatrice/Application.java
+++ src/main/java/calculatrice/Application.java
@@ -5,99 +5,99 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application {
-	/**
-	 * Fonction de démarrage du programme
-	 */
+    /**
+     * Fonction de démarrage du programme
+     */
     public static void main(String[] args) {
 
-		ICalculatrice vCalculator = new Calculatrice();
-		ArrayList<String> histoList = new ArrayList<String>();
-		Scanner sc = new Scanner(System.in);
-		boolean estCalculable = true;
-		String premiereValeur = null;
-		String deuxiemeValeur = null;
-		String resultat = null;
-		String calcul = null;
-		Fichier fichier = new Fichier();
+        ICalculatrice vCalculator = new Calculatrice();
+        ArrayList<String> histoList = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+        boolean estCalculable = true;
+        String premiereValeur = null;
+        String deuxiemeValeur = null;
+        String resultat = null;
+        String calcul = null;
+        Fichier fichier = new Fichier();
 
-		do {
-			calcul = null;
-			premiereValeur = null;
-			deuxiemeValeur = null;
-			resultat = null;
-			System.out.println("Choissisez l'opÃ©ration (+/-/*), h pour l'historique ou q pour quitter ! ");
-			String operation = sc.nextLine();
+        do {
+            calcul = null;
+            premiereValeur = null;
+            deuxiemeValeur = null;
+            resultat = null;
+            System.out.println("Choissisez l'opération (+/-/*), h pour l'historique ou q pour quitter ! ");
+            String operation = sc.nextLine();
 
-			switch (operation) {
-			case Constantes.QUIT_APPLI:
-				System.out.println("ArrÃªt de la calculatrice");
-				System.exit(0);
-				break;
+            switch (operation) {
+            case Constantes.QUIT_APPLI:
+                System.out.println("Arret de la calculatrice");
+                System.exit(0);
+                break;
 
-			case Constantes.ADD_KEY:
-				System.out.println("Valeur 1 : ");
-				premiereValeur = sc.nextLine();
-				System.out.println("Valeur 2: ");
-				deuxiemeValeur = sc.nextLine();
+            case Constantes.ADD_KEY:
+                System.out.println("Valeur 1 : ");
+                premiereValeur = sc.nextLine();
+                System.out.println("Valeur 2: ");
+                deuxiemeValeur = sc.nextLine();
 
-				resultat = Constantes.EMPTY;
+                resultat = Constantes.EMPTY;
 
-				if (!premiereValeur.isEmpty() & !deuxiemeValeur.isEmpty()) {
-					resultat = vCalculator.addition(premiereValeur, deuxiemeValeur);
-					System.out.println("Le rÃ©sultat est : ");
-					System.out.println(resultat);
-				} else {
-					System.out.println("erreur dans la saisie");
-				}
-				break;
+                if (!premiereValeur.isEmpty() & !deuxiemeValeur.isEmpty()) {
+                    resultat = vCalculator.addition(premiereValeur, deuxiemeValeur);
+                    System.out.println("Le résultat est : ");
+                    System.out.println(resultat);
+                } else {
+                    System.out.println("erreur dans la saisie");
+                }
+                break;
 
-			case Constantes.SUB_KEY:
-				System.out.println("Valeur 1 : ");
-				premiereValeur = sc.nextLine();
-				System.out.println("Valeur 2: ");
-				deuxiemeValeur = sc.nextLine();
-				resultat = Constantes.EMPTY;
+            case Constantes.SUB_KEY:
+                System.out.println("Valeur 1 : ");
+                premiereValeur = sc.nextLine();
+                System.out.println("Valeur 2: ");
+                deuxiemeValeur = sc.nextLine();
+                resultat = Constantes.EMPTY;
 
-				if (!premiereValeur.isEmpty() & !deuxiemeValeur.isEmpty()) {
-					resultat = vCalculator.soustraction(premiereValeur, deuxiemeValeur);
-					System.out.println("Le rÃ©sultat est : ");
-					System.out.println(resultat);
-				} else {
-					System.out.println("erreur dans la saisie");
-				}
-				break;
+                if (!premiereValeur.isEmpty() & !deuxiemeValeur.isEmpty()) {
+                    resultat = vCalculator.soustraction(premiereValeur, deuxiemeValeur);
+                    System.out.println("Le résultat est : ");
+                    System.out.println(resultat);
+                } else {
+                    System.out.println("erreur dans la saisie");
+                }
+                break;
 
-			case Constantes.HISTO:
-				resultat = Constantes.EMPTY;
-				fichier.lecture();
-				break;
-			case Constantes.MUL_KEY:
-				System.out.println("Valeur 1 : ");
-				premiereValeur = sc.nextLine();
-				System.out.println("Valeur 2: ");
-				deuxiemeValeur = sc.nextLine();
-				resultat = Constantes.EMPTY;
+            case Constantes.HISTO:
+                resultat = Constantes.EMPTY;
+                fichier.lecture();
+                break;
+            case Constantes.MUL_KEY:
+                System.out.println("Valeur 1 : ");
+                premiereValeur = sc.nextLine();
+                System.out.println("Valeur 2: ");
+                deuxiemeValeur = sc.nextLine();
+                resultat = Constantes.EMPTY;
 
-				if (!premiereValeur.isEmpty() & !deuxiemeValeur.isEmpty()) {
-					resultat = vCalculator.multiplication(premiereValeur, deuxiemeValeur);
-					System.out.println("Le rÃ©sultat est : ");
-					System.out.println(resultat);
-				} else {
-					System.out.println("erreur dans la saisie");
-				}
+                if (!premiereValeur.isEmpty() & !deuxiemeValeur.isEmpty()) {
+                    resultat = vCalculator.multiplication(premiereValeur, deuxiemeValeur);
+                    System.out.println("Le résultat est : ");
+                    System.out.println(resultat);
+                } else {
+                    System.out.println("erreur dans la saisie");
+                }
 
-				break;
+                break;
 
-			default:
-				System.out.println("opération non disponible");
-				break;
+            default:
+                System.out.println("opération non disponible");
+                break;
 
-			}
-			calcul = premiereValeur + " " + operation + " " + deuxiemeValeur + " = "+resultat;
-			if((calcul != null) & (operation != null & operation != Constantes.HISTO) & (premiereValeur != null) & (deuxiemeValeur != null) & (!resultat.contains(" "))) {
-				histoList.add(calcul);
-				fichier.ecritureApplication(histoList);
-			}
-		} while (estCalculable);
-	}
+            }
+            calcul = premiereValeur + " " + operation + " " + deuxiemeValeur + " = "+resultat;
+            if((calcul != null) & (operation != null & operation != Constantes.HISTO) & (premiereValeur != null) & (deuxiemeValeur != null) & (!resultat.contains(" "))) {
+                histoList.add(calcul);
+                fichier.ecritureApplication(histoList);
+            }
+        } while (estCalculable);
+    }
 }
--- src/main/java/calculatrice/Constantes.java
+++ src/main/java/calculatrice/Constantes.java
@@ -4,26 +4,26 @@ public class Constantes {
 
 public static final String EMPTY = "";
 
-	/** Le separateur de représentation pour le float. */
-	public static final String FLOAT_SEPARATOR = ".";
+    /** Le separateur de représentation pour le float. */
+    public static final String FLOAT_SEPARATOR = ".";
 
-	/** La cle pour l'opération d'addition. */
-	public static final String ADD_KEY = "+";
+    /** La cle pour l'opération d'addition. */
+    public static final String ADD_KEY = "+";
 
-	/** La cle pour l'opération soustraction. */
-	public static final String SUB_KEY = "-";
+    /** La cle pour l'opération soustraction. */
+    public static final String SUB_KEY = "-";
 
-	/** La cle pour l'opération division. */
-	public static final String DIVIDE_KEY = "/";
+    /** La cle pour l'opération division. */
+    public static final String DIVIDE_KEY = "/";
 
-	/** La cle pour l'opération multiplication. */
-	public static final String MUL_KEY = "*";
+    /** La cle pour l'opération multiplication. */
+    public static final String MUL_KEY = "*";
 
-	/** La cle pour pour quitter l'application. */
-	public static final String QUIT_APPLI = "q";
+    /** La cle pour pour quitter l'application. */
+    public static final String QUIT_APPLI = "q";
 
-	/** La cle pour l'historique */
-	public static final String HISTO = "h";
+    /** La cle pour l'historique */
+    public static final String HISTO = "h";
 
 
 }
